fc72ff9c38be9548747461767b68ae87c4f5bfd3
Melvin González <54681730+melagora@users.noreply.github.com>
2023-09-26 22:55:17 -0600

    Ya somos STATELESS
    Tenemos el control de la autenticación

--- src/main/java/med/voll/api/infra/security/AutenticacionService.java
+++ src/main/java/med/voll/api/infra/security/AutenticacionService.java
@@ -16,7 +16,7 @@ public class AutenticacionService implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByUsername(username);
+        return usuarioRepository.findByLogin(username);
     }
     
 }
