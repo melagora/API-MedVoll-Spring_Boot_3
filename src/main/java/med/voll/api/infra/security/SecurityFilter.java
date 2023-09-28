fadf62608211cdd9df1598d43341124031377e96
Melvin González <54681730+melagora@users.noreply.github.com>
2023-09-28 16:50:22 -0600

    Creando el Security Filter
    Se creo el filtro, se intercepta el request y se llama al siguiente filtro para mostrar la información.

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,21 @@
+package med.voll.api.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException{
+        System.out.println("El filtro esta siendo llamado");
+        filterChain.doFilter(request, response);
+    }
+
+}
