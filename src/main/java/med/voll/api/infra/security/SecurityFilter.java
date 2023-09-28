9dc5c2efdf018bbe213c7813b0c6eb81ee76ac1e
Melvin González <54681730+melagora@users.noreply.github.com>
2023-09-28 17:51:53 -0600

    Validando el Token - ERROR -
    Validando si el token no ha expirado y pertenece al usuario que ha iniciado sesion

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +13,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException{
 
@@ -22,6 +27,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
         token = token.replace("Bearer", "");
         System.out.println(token);
+        System.out.println(tokenService.getSubject(token));
         filterChain.doFilter(request, response);
     }
 
--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -10,27 +10,47 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import med.voll.api.domain.usuarios.Usuario;
 
 @Service
 public class TokenService {
-
+//
     @Value("${api.security.secret}")
     private String apiSecret;
-    
+
     public String generarToken(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(apiSecret);
             return JWT.create()
-            .withIssuer("voll med")
-            .withSubject(usuario.getLogin())
-            .withClaim("id", usuario.getId())
-            .withExpiresAt(generarFechaExpiracion())
-            .sign(algorithm);
+                    .withIssuer("voll med")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException();
         }
+    }
+
+    public String getSubject(String token) {
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer("voll med")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if(verifier.getSubject() == null){
+            throw new RuntimeException("Verifer invalido");
+        }
+         return verifier.getSubject();
     }
 
     private Instant generarFechaExpiracion(){
